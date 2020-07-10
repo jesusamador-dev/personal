78a5af8f69ad40d41533d74655fa7881a1a6b382
Jesús Guadalupe Macías Amador <jesus.macias.amador@gmail.com>
2020-07-09 22:01:31 -0500

    Deploy

---
+++ scripts/untar.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+set -x
+export NODE_ENV=production
+export NVM_BIN=$HOME/.nvm/versions/node/v12.18.1/bin
+cd /var/www/test.jesusamador.com/html/ && \
+tar zxvf package.tgz -C . && \
+mv build/package.json . && \
+# npm install && \
+# npm run start
