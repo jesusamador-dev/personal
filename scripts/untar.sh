a488ed4befe0270d892a917208ea645231e50f38
Jesús Guadalupe Macías Amador <jesus.macias.amador@gmail.com>
2020-07-12 23:36:12 -0500

    Cambiando script de deploy

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -4,7 +4,7 @@ export NODE_ENV=production
 export NVM_BIN=$HOME/.nvm/versions/node/v12.18.1/bin
 cd /var/www/test.jesusamador.com/html/ && \
 tar zxvf package.tgz -C . && \
-mv personal/* /var/www/test.jesusamador.com/html && \
+rsync -a personal/ /var/www/test.jesusamador.com/html && \
 # mv build/package.json . && \
 npm install
 # && \
